3c9d6839ee6066674d043b1ee73445c531971c25
hzg0502 <3117406921@qq.com>
2026-03-07 14:40:01 +0800

    后端初始化

---
+++ src/test/java/com/guyue/gupicturebackend/GuPictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guyue.gupicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuPictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
